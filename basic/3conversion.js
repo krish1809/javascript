05d5b6a13d2dadd7053052ce0fc7ca53507f04b8
krish1809 <158037305+krish1809@users.noreply.github.com>
2025-05-09 07:26:05 +0000

    converions 2

---
+++ basic/3conversion.js
@@ -0,0 +1,29 @@
+// let age =2
+// let valueInNumber = Number(age)
+// // "33" => 33
+// // "33abc" => NaN
+// //  true => 1; false => 0
+
+// let isLoggedIn = "hitesh"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// // 1 => true; 0 => false
+// // "" => false
+// // "hitesh" => true
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+
+// // no problem in string
+
+
+// // *********************** Operations ***********************
+
+console.log("1"+1)
+console.log(1+"2")
+console.log(1+2+"3")
+
+
+
